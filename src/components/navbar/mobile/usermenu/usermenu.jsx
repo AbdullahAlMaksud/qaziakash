52aeeb149c9b3d8673ba5dbac028518980112844
Abdullah Al Maksud <maksud.workspace@gmail.com>
2024-12-31 10:04:11 +0600

    setup nextjs, tailwind and shadcn; add:mobile navbar

---
+++ src/components/navbar/mobile/usermenu/usermenu.jsx
@@ -0,0 +1,40 @@
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { MenuSquare } from "lucide-react";
+
+const Usermenu = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline" className="rounded-none">
+          <MenuSquare />
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Edit profile</SheetTitle>
+          <SheetDescription>
+            Make changes to your profile here. Click save when you're done.
+          </SheetDescription>
+        </SheetHeader>
+        <div className="grid gap-4 py-4"></div>
+        <SheetFooter>
+          <SheetClose asChild>
+            <Button type="submit">Save changes</Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default Usermenu;
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import { Button } from "../ui/button";
+import Usermenu from "./mobile/usermenu/usermenu";
+
+const Navbar = () => {
+  return (
+    <section className="bg-transparent fixed w-full min-h-14 max-h-14 z-50 flex items-center">
+      <section className="md:container w-11/12 mx-auto px-2 flex items-center justify-between">
+        {/* Logo */}
+
+        <div className="">
+          <div className="flex items-center gap-2 ">
+            <Image
+              src="/icon.svg"
+              width={40}
+              height={100}
+              alt="icon"
+              className="w-7"
+            ></Image>
+            <h2 className="font-english text-white font-medium text-xl">
+              Qazi Akash
+            </h2>
+          </div>
+        </div>
+
+        {/* Social Links */}
+
+        {/* Nav Links */}
+        {/* Button */}
+        <section className="flex items-center gap-1">
+          <Button className="font-english rounded-none">Get Started</Button>
+          <Usermenu />
+        </section>
+        {/* Navmenu */}
+      </section>
+    </section>
+  );
+};
+
+export default Navbar;
