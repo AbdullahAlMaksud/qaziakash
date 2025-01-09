dac68ac5e8b75b4e105d31bafc4572fe45115407
Abdullah Al Maksud <maksud.workspace@gmail.com>
2025-01-10 00:09:43 +0600

    add: blogs ui

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,20 +1,71 @@
 "use client";
 import { useBlogs } from "@/hooks/use-queries";
+import Image from "next/image";
+import Link from "next/link";
 
 const Blogs = () => {
-  const { data, isLoading, error } = useBlogs();
-  console.log(data);
+  const { data: posts, isLoading, error } = useBlogs();
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString("en-GB", {
+      day: "numeric",
+      month: "2-digit",
+      year: "numeric",
+    });
+  };
+
   if (error) return <div>Error loading blogs</div>;
 
   return (
     <section className="-mt-16">
       <div className="flex bg-deepblue pt-32 pb-16 flex-col items-center font-english text-white">
         <h2 className="mb-4 text-center text-4xl lg:text-5xl font-bold">
-          Book Store{" "}
+          Blogs
         </h2>
         <hr className="w-10 h-0.5 bg-primary mb-8 lg:mb-14" />
       </div>
-      {data?.length}
+      <section className="flex  items-center justify-center w-11/12 mx-auto my-20">
+        <div className="flex flex-col lg:flex-row gap-4 flex-wrap justify-center">
+          {posts?.map((post) => (
+            <div
+              key={post.id}
+              className="w-[20rem] bg-white flex flex-col hover:shadow-sm"
+            >
+              <Image
+                src={post.coverImage}
+                alt=""
+                height={100}
+                width={100}
+                className="w-[20rem] h-60 object-cover"
+              />
+
+              <div className="px-8 py-7 flex flex-col flex-1">
+                <p className="mb-3 text-2xl h-[3.9rem] text-deepblue font-bold overflow-clip">
+                  {post.title}
+                </p>
+                <p className="font-inter text-gray-500 flex-1">
+                  {post.excerpt.slice(0, 100)}...
+                </p>
+                <div className="flex mt-4 justify-between">
+                  <Link
+                    className="border-b-2 border-deepblue text-deepblue font-bold font-bangla"
+                    href={"/"}
+                  >
+                    আরও পড়ুন
+                  </Link>
+                  <p className="font-bangla text-sm">
+                    {post?.author} -{" "}
+                    <span className="font-['kalpurush']">
+                      {formatDate(post.publishDate)}
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     </section>
   );
 };
