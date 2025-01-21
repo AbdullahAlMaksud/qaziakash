8859f3e62b94e8a54eaaad36d7fd012721ea9804
Abdullah Al Maksud <maksud.workspace@gmail.com>
2025-01-21 17:10:35 +0600

    add: comming soon page...

--- src/pages/commingSoon/commingSoon.jsx
+++ src/pages/commingSoon/commingSoon.jsx
@@ -2,13 +2,22 @@
 import { useFetch } from "@/hooks/useFetch";
 import Link from "next/link";
 import { CiAt, CiFacebook } from "react-icons/ci";
+import Typewriter from "typewriter-effect";
 
 export default function ComingSoon() {
   const { books } = useFetch("/data/booksData.json");
   return (
     <section className="min-h-screen flex items-center justify-center flex-col relative">
       <div className="bg-secondary w-full flex items-center justify-center py-20 flex-col">
-        <h2 className="font-bangla text-4xl font-bold">শিগগিরই আসছে...</h2>
+        <h2 className="font-bangla text-4xl font-bold">
+          <Typewriter
+            options={{
+              strings: ["শিগগিরই আসছে...", "অপেক্ষা করুন!", "নতুন কিছু আসছে!"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </h2>
         <div className="flex items-center gap-2 pt-2">
           <Link
             className="font-english font-extralight text-sm text-black"
@@ -17,7 +26,7 @@ export default function ComingSoon() {
             qaziakash.com
           </Link>
           <hr className="w-[.1rem] min-h-4 bg-black " />
-          <div className="border flex p-0.5 border-black">
+          <div className="border flex gap-1 lg:gap-1 p-0.5 border-black">
             <Link
               className="font-english font-extralight text-sm"
               href={"https://www.facebook.com/"}
